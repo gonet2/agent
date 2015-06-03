479fffe050db3cee76a5de65c7aaddd340afb7a4
xtaci <daniel820313@gmail.com>
2015-06-03 13:27:27 +0000

    update

---
+++ src/services/def.go
@@ -0,0 +1,10 @@
+package services
+
+type ServiceType string
+
+const (
+	SERVICE_SNOWFLAKE  = ServiceType("/backends/snowflake")
+	SERVICE_GEOIP      = ServiceType("/backends/geoip")
+	SERVICE_WORDFILTER = ServiceType("/backends/wordfilter")
+	SERVICE_BGSAVE     = ServiceType("/backends/bgsave")
+)
