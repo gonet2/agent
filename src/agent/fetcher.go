bab9e0f448116e8e90dc5a2a98db3a11355c8607
luo Jun <johnnyluo586@gmail.com>
2015-07-01 12:05:17 +0800

    rename

--- src/agent/fetcher.go
+++ src/agent/fetcher.go
@@ -1,9 +1,10 @@
 package main
 
 /*
- * 长连接 server streaming RPC 到game servers, 获得异步消息返回客户端
+ * 长连接 server streaming RPC 到game servers, 获得异步消息返回客户端, RPC  game server Notify
  */
 
 // message fetcher
 func fetcher_task() {
+
 }
--- src/agent/forward.go
+++ src/agent/forward.go
@@ -12,6 +12,11 @@ var (
 	ERROR_NOT_AUTHORIZED = errors.New("User not authorized")
 )
 
+/*
+ * 长连接 双向流 RPC 到game servers, 转发agent <==> game 消息
+ */
+
+// message forward
 func forward(sess *Session, p []byte) ([]byte, error) {
 	if sess.Flag&SESS_AUTHORIZED != 0 {
 		// TODO: forward packet to a game server
